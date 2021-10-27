087396380dfdd66579a7edaf506bf894c02a6b5c
David Duthie <dave.a.duthie@gmail.com>
2021-10-27 09:11:52 +0200

    get mustache-rs working again

--- cljs-app/src/hello_world/core.cljs
+++ cljs-app/src/hello_world/core.cljs
@@ -1,9 +1,10 @@
 (ns hello-world.core
   (:require
    [clojure.core.async :refer [<! go]]
+   [reagent.core :as r]
    [clojure.core.async.interop :refer-macros [<p!]]
    [goog.dom :as gdom]
-   [mustache-rs]
+   ["mustache-rs/mustache_rs" :as mustache-rs]
    [reagent.dom :as dom]))
 
 (defn set-context [ctx]
@@ -13,17 +14,26 @@
   [str]
   (go (.new (.-Mustache (<p! mustache-rs)) str)))
 
-(defn test-mustache
+(defn mustache-test
   []
-  (go (let [tmpls [(<! (template "Hello {{ greet.who }}"))
-                   (<! (template "Hello {{ greet.whoElse }}"))]]
-        (<! (set-context #js {:greet #js {:who "The Who", :whoElse "Elsie"}}))
-        (prn :deps (set (mapcat #(.deps % #js ["greet"]) tmpls)))
-        (prn (mapv #(.render %) tmpls)))))
+  (let [*state (r/atom nil)]
+    (go (let [tmpls [(<! (template "Hello {{ calc.who }}"))
+                     (<! (template "Hello {{ calc.whoElse }} {{ calc.doesnt.resolve.to.anything }}"))]]
+          (<! (set-context #js {:calc #js {:who "The Who", :whoElse "Elsie"}}))
+          (reset! *state
+                  (map (fn [tmpl]
+                         {:deps     (set (.deps tmpl #js ["calc"]))
+                          :rendered (.render tmpl)})
+                       tmpls))))
+    (fn []
+      [:ul
+       (doall (map (fn [{:keys [deps rendered]}]
+                     [:li {:key rendered} rendered " "
+                      [:code (pr-str deps)]]) @*state))])))
 
 (defn simple-component []
   [:div
    [:p "CLJS + Wasm Hello World"]
-   [:button {:on-click #(test-mustache)} "Mustache"]])
+   [mustache-test]])
 
 (dom/render [simple-component] (gdom/getElement "app"))
